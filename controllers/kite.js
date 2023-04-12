cdf16a5812643d229ffcae177a335f15379b172b
narasimhamadasu <123008253+narasimhamadasu@users.noreply.github.com>
2023-04-12 00:50:55 -0500

    DONE

--- controllers/kite.js
+++ controllers/kite.js
@@ -8,9 +8,27 @@ exports.kite_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: kite detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.kite_create_post = function(req, res) {
-res.send('NOT IMPLEMENTED: kite create POST');
-};
+exports.kite_create_post =async function(req, res) {
+    console.log(req.body)
+    let document = new kite();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.kite_color = req.body.kite_color;
+    document.kite_quality = req.body.kite_quality;
+    document.kite_cost = req.body.kite_cost;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
+
 // Handle Costume delete form on DELETE.
 exports.kite_delete = function(req, res) {
 res.send('NOT IMPLEMENTED: kite delete DELETE ' + req.params.id);
@@ -29,4 +47,17 @@ exports.kite_list = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+// VIEWS
+// Handle a show all view
+exports.kite_view_all_Page = async function(req, res) {
+    try{
+    thekite = await kite.find();
+    res.render('kite', { title: 'kite Search Results', results: thekite });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+    };
+    
     
--- routes/kite.js
+++ routes/kite.js
@@ -1,10 +1,11 @@
 var express = require('express');
+const kite_controlers= require('../controllers/kite');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('kite', { title: 'Search Results kite' });
-});
-
-
+/* GET costumes */
+router.get('/', kite_controlers.kite_view_all_Page );
 module.exports = router;
+
+
+
+
