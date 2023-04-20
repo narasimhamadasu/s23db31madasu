cadb929c05eb9595dc5cde16448dd0b32679cf9f
narasimhamadasu <123008253+narasimhamadasu@users.noreply.github.com>
2023-04-19 21:16:53 -0500

    done

--- controllers/kite.js
+++ controllers/kite.js
@@ -7,6 +7,18 @@ res.send('NOT IMPLEMENTED: kite list');
 exports.kite_detail = function(req, res) {
 res.send('NOT IMPLEMENTED: kite detail: ' + req.params.id);
 };
+// for a specific Costume.
+exports.kite_detail = async function(req, res) {
+    console.log("detail" + req.params.id)
+    try {
+    result = await kite.findById( req.params.id)
+    res.send(result)
+    } catch (error) {
+    res.status(500)
+    res.send(`{"error": document for id ${req.params.id} not found`);
+    }
+    };
+    
 // Handle Costume create on POST.
 exports.kite_create_post =async function(req, res) {
     console.log(req.body)
@@ -37,6 +49,26 @@ res.send('NOT IMPLEMENTED: kite delete DELETE ' + req.params.id);
 exports.kite_update_put = function(req, res) {
 res.send('NOT IMPLEMENTED: kite update PUT' + req.params.id);
 };
+//Handle Costume update form on PUT.
+exports.kite_update_put = async function(req, res) {
+console.log(`update on id ${req.params.id} with body
+${JSON.stringify(req.body)}`)
+try {
+let toUpdate = await kite.findById( req.params.id)
+// Do updates of properties
+if(req.body.kite_color)
+toUpdate.kite_color = req.body.kite_color;
+if(req.body.kite_quality) toUpdate.kite_quality = req.body.kite_quality;
+if(req.body.kite_cost) toUpdate.kite_cost = req.body.kite_cost;
+let result = await toUpdate.save();
+console.log("Sucess " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": ${err}: Update for id ${req.params.id}
+failed`);
+}
+};
 exports.kite_list = async function(req, res) {
     try{
     thekite = await kite.find();
@@ -59,5 +91,91 @@ exports.kite_view_all_Page = async function(req, res) {
     res.send(`{"error": ${err}}`);
     }
     };
+
+    // Handle Costume delete on DELETE.
+exports.kite_delete = async function(req, res) {
+console.log("delete " + req.params.id)
+try {
+result = await kite.findByIdAndDelete( req.params.id)
+console.log("Removed " + result)
+res.send(result)
+} catch (err) {
+res.status(500)
+res.send(`{"error": Error deleting ${err}}`);
+}
+};
+
+
+// Handle a show one view with id specified by query
+exports.kite_view_one_Page = async function(req, res) {
+    console.log("single view for id " + req.query.id)
+    try{
+    result = await kite.findById( req.query.id)
+    res.render('kitedetail',
+    { title: 'kite Detail', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for creating a costume.
+// No body, no in path parameter, no query.
+// Does not need to be async
+exports.kite_create_Page = function(req, res) {
+    console.log("create view")
+    try{
+    res.render('kitecreate', { title: 'kite Create'});
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle building the view for updating a costume.
+// query provides the id
+exports.kite_update_Page = async function(req, res) {
+console.log("update view for item "+req.query.id)
+try{
+result = await kite.findById(req.query.id)
+res.render('kiteupdate', { title: 'kite Update', toShow: result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+// Handle building the view for updating a costume.
+// query provides the id
+exports.kite_update_Page = async function(req, res) {
+    console.log("update view for item "+req.query.id)
+    try{
+    let result = await Costume.findById(req.query.id)
+    res.render('costumeupdate', { title: 'Costume Update', toShow: result });
+    }
+    catch(err){
+    res.status(500)
+    res.send(`{'error': '${err}'}`);
+    }
+    };
+
+    // Handle a delete one view with id from query
+exports.kite_delete_Page = async function(req, res) {
+console.log("Delete view for id " + req.query.id)
+try{
+result = await kite.findById(req.query.id)
+res.render('kitedelete', { title: 'kite Delete', toShow:
+result });
+}
+catch(err){
+res.status(500)
+res.send(`{'error': '${err}'}`);
+}
+};
+
+
     
     
--- routes/kite.js
+++ routes/kite.js
@@ -4,7 +4,23 @@ var router = express.Router();
 
 /* GET costumes */
 router.get('/', kite_controlers.kite_view_all_Page );
+
+/* GET detail costume page */
+router.get('/detail', kite_controlers.kite_view_one_Page);
+
+/* GET create costume page */
+router.get('/create', kite_controlers.kite_create_Page);
+
+/* GET create update page */
+router.get('/update', kite_controlers.kite_update_Page);
+
+/* GET delete costume page */
+router.get('/delete', kite_controlers.kite_delete_Page);
+
+
+
 module.exports = router;
+
 
 
 
